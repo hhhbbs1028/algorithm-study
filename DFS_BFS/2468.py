4ac210beda56842a5977805472f2d1229db91e5a
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2025-06-23 11:12:24 +0900

    06/23

--- DFS_BFS/2468.py
+++ DFS_BFS/2468.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(100000)
+
 n = int(input())
 
 graph = []
@@ -7,7 +10,7 @@ for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 
-def dfs(i, j):
+def dfs(i, j, water):
     x = [1,-1,0,0]
     y = [0,0,1,-1]
     visited[i][j]=True
@@ -15,36 +18,30 @@ def dfs(i, j):
     for d in range(4):
         dx=i+x[d]
         dy=j+y[d]
-        if 0<=dx<n and 0<=dy<n and not visited[dx][dy]:
-            dfs(dx, dy)
+        if 0<=dx<n and 0<=dy<n and not visited[dx][dy] and graph[dx][dy]>water:
+            dfs(dx, dy, water)
 
 
 def count_map(graph, water):
     count=0
     for i in range(n):
         for j in range(n):
-            if graph[i][j]>water:
-                "수위 넘음"
-            if not visited[i][j]:
-                "not visited"
             if graph[i][j]>water and not visited[i][j]:
-                print("수위 ",water, "일 때 1 count 1증가", count)
-                dfs(i, j)
+                dfs(i, j, water)
                 count+=1
     return count
 
 m_water = set()
+m_water.add(0)
 for i in range(n):
     for j in range(n):
         if graph[i][j] not in m_water:
             m_water.add(graph[i][j])
 
-print(m_water)
-c=0
+c=1
 for w in m_water:
     visited = [[False]*n for _ in range(n)]
     c_m = count_map(graph, w)
-    print("count : ", c_m)
     if c<c_m:
         c=c_m
 print(c)
