b6d252b0a3749c79b956b101addbf0df0a85f5fc
jaehee kwon <100170572+hhhbbs1028@users.noreply.github.com>
2026-03-06 02:15:19 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 3940 ms, Memory: 127420 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,9 @@
+n = int(input())
+nums = [0]*10001
+for _ in range(n):
+    nums[int(input())]+=1
+
+for i in range(10001):
+    if nums[i]!=0:
+        for j in range(nums[i]):
+           print(i)    
