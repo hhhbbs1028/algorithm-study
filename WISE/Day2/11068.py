5573db147dd238ae65bbd0edd146c2cc0b1c41c5
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2026-03-07 23:26:23 +0900

    solve : [BOJ] 11068 회문인 수

--- WISE/Day2/11068.py
+++ WISE/Day2/11068.py
@@ -1,25 +1,27 @@
-def is_palindrome(x, base):
-    
-    digit = []
-    
-    while(x>0):
-        digit.append(x%base)
-        x//base
-    for i in range(0, len(digit)//2):
-        if digit[i] != digit[len(digit)-i-1]:
-            return False
-    return True
-        
-
+# 어떤 B진법 (2 ≤ B ≤ 64)으로 표현하면 회문이 되는 경우가 있는지 알려주는 프로그램
+def cal(number, B):
+    # number를 B진수로 변환
+    n = []
+    while True:
+        n.append(number%B)
+        number=number//B
+        if number < B:
+            n.append(number)
+            break
+    n.reverse()
+    return n
 
 t = int(input())
 for _ in range(t):
-    n = int(input())
-    ans = 0
+    flag = 0
+    num = int(input())
+    for i in range(64, 1, -1):
+        cal_num = cal(num, i)
+        # print(i, cal_num)
+        if cal_num == cal_num[::-1]:
+            flag = 1
+            break
+    print(flag)
     
-    for i in range(2, 65):
-        if is_palindrome(n, i):
-            ans = 1
-            break 
-    print(ans)
-
+# reverse 비교 => list == list[::-1]
+# 슬라이싱 : [start:stop:step]
