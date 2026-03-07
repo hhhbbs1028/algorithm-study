1f0bc0770f770c83ed94f61a7be165b8c382be7e
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2026-03-07 23:26:02 +0900

    solve : [BOJ] 3273 두 수의 합

--- WISE/Day2/3273.py
+++ WISE/Day2/3273.py
@@ -1,12 +1,12 @@
 n = int(input())
 nums = list(map(int, input().split()))
 x = int(input())
-
+nums_all = [0]*1000001
 count=0
-# print(nums)
-for a in nums:
-    b = x-a
-    if b in nums and a<b:
-        count+=1
-        
+for num in nums:
+    nums_all[num]+=1
+for num in nums:
+    if x-num>0 and num>(x//2):
+        if nums_all[x-num]!=0:
+            count+=1
 print(count)
