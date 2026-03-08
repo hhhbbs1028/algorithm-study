7dc5ebca57348e1993dc8aad34e0ee26385d54df
jaehee kwon <100170572+hhhbbs1028@users.noreply.github.com>
2026-03-08 20:20:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2920 ms, Memory: 127164 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,11 +1,9 @@
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
-nums.insert(0,0)
-acc = [0]*(n+1)
-for i in range(1, n+1):
-    acc[i] = acc[i-1]+nums[i]
+sums = [0]
+for k in range(n):
+    sums.append(sums[k]+nums[k])
 
-for _ in range(m):
+for _ in range(m):    
     i, j = map(int, input().split())
-    print(acc[j]-acc[i-1])
-
+    print(sums[j]-sums[i-1])
