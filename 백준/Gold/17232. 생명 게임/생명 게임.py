8cf0fd941c9d35584c816a68d140d4bd15453cb6
jaehee kwon <100170572+hhhbbs1028@users.noreply.github.com>
2026-03-11 03:38:53 +0900

    [Gold IV] Title: 생명 게임, Time: 696 ms, Memory: 133852 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17232. 생명 게임/생명 게임.py
+++ 백준/Gold/17232. 생명 게임/생명 게임.py
@@ -1,12 +1,20 @@
 import sys
+import copy
 input = sys.stdin.readline
 n, m, t = map(int, input().split())
 k, a, b = map(int, input().split())
-k = min(k, max(n,m))
-
+k = min(k, max(m, n))
+    
 life = []
 for _ in range(n):
     life.append(list(input().strip()))
+
+def show_graph(graph):
+    for i in range(n):
+        for j in range(m):
+            print(graph[i][j], end="")
+        print()
+    print()
 
 def plus_count(x, y):
     for i in range(-k, k+1):
@@ -15,14 +23,15 @@ def plus_count(x, y):
                 if 0<=y+j<m:
                     count_life[x+i][y+j]+=1
     count_life[x][y]-=1
-    
-def show_graph(graph):
-    for i in range(n):
-        for j in range(m):
-            print(graph[i][j], end="")
-        print()
-    print()
 
+def minus_count(x, y):
+    for i in range(-k, k+1):
+        if 0<=x+i<n:
+            for j in range(-k, k+1):
+                if 0<=y+j<m:
+                    count_life[x+i][y+j]-=1
+    count_life[x][y]+=1
+    
 def born_life(graph):
     for i in range(n):
         for j in range(m):
@@ -33,13 +42,24 @@ def born_life(graph):
                 if a<count_life[i][j]<=b:
                     graph[i][j]='*'
 
-for _ in range(t):
-    count_life = [[0]*m for _ in range(n)]
+count_life = [[0]*m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if life[i][j]=='*':
+            plus_count(i, j)
+    
+for _ in range(t):  
+    # born_life(life)
+    temp_count = copy.deepcopy(count_life)
     for i in range(n):
         for j in range(m):
             if life[i][j]=='*':
-                plus_count(i, j)
-                
-    born_life(life)
+                if (temp_count[i][j]<a) or (temp_count[i][j]>b):
+                    life[i][j]='.'
+                    minus_count(i,j)
+            else:
+                if a<temp_count[i][j]<=b:
+                    life[i][j]='*'
+                    plus_count(i,j)
     # show_graph(count_life)
 show_graph(life)
