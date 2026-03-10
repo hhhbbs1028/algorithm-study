2122da3c4b51f8ae993e22471a3c97d65af885db
jaehee kwon <100170572+hhhbbs1028@users.noreply.github.com>
2026-03-11 03:57:20 +0900

    [Gold IV] Title: 생명 게임, Time: 616 ms, Memory: 149716 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17232. 생명 게임/생명 게임.py
+++ 백준/Gold/17232. 생명 게임/생명 게임.py
@@ -1,65 +1,41 @@
 import sys
-import copy
 input = sys.stdin.readline
+
 n, m, t = map(int, input().split())
 k, a, b = map(int, input().split())
-k = min(k, max(m, n))
-    
+
 life = []
 for _ in range(n):
     life.append(list(input().strip()))
 
-def show_graph(graph):
+def calc_count():
+    count = [[0]*m for _ in range(n)]
     for i in range(n):
         for j in range(m):
-            print(graph[i][j], end="")
-        print()
-    print()
+            if life[i][j] == '*':
+                for di in range(-k, k+1):
+                    for dj in range(-k, k+1):
+                        if di==0 and dj==0:
+                            continue
+                        ni, nj = i+di, j+dj
+                        if 0<=ni<n and 0<=nj<m:
+                            count[ni][nj] += 1
+    return count
 
-def plus_count(x, y):
-    for i in range(-k, k+1):
-        if 0<=x+i<n:
-            for j in range(-k, k+1):
-                if 0<=y+j<m:
-                    count_life[x+i][y+j]+=1
-    count_life[x][y]-=1
-
-def minus_count(x, y):
-    for i in range(-k, k+1):
-        if 0<=x+i<n:
-            for j in range(-k, k+1):
-                if 0<=y+j<m:
-                    count_life[x+i][y+j]-=1
-    count_life[x][y]+=1
+for _ in range(t):
+    count = calc_count()
+    new_life = [row[:] for row in life]  # 얕은 복사로 충분 (1depth)
     
-def born_life(graph):
     for i in range(n):
         for j in range(m):
-            if graph[i][j]=='*':
-                if (count_life[i][j]<a) or (count_life[i][j]>b):
-                    graph[i][j]='.'
+            if life[i][j] == '*':
+                if count[i][j] < a or count[i][j] > b:
+                    new_life[i][j] = '.'
             else:
-                if a<count_life[i][j]<=b:
-                    graph[i][j]='*'
-
-count_life = [[0]*m for _ in range(n)]
-for i in range(n):
-    for j in range(m):
-        if life[i][j]=='*':
-            plus_count(i, j)
+                if a < count[i][j] <= b:
+                    new_life[i][j] = '*'
     
-for _ in range(t):  
-    # born_life(life)
-    temp_count = copy.deepcopy(count_life)
-    for i in range(n):
-        for j in range(m):
-            if life[i][j]=='*':
-                if (temp_count[i][j]<a) or (temp_count[i][j]>b):
-                    life[i][j]='.'
-                    minus_count(i,j)
-            else:
-                if a<temp_count[i][j]<=b:
-                    life[i][j]='*'
-                    plus_count(i,j)
-    # show_graph(count_life)
-show_graph(life)
+    life = new_life
+
+for row in life:
+    print("".join(row))
