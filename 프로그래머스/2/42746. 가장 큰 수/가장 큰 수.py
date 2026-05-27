a16dc07ffea7d06fbe22a2e8c0a6fc5e4f3e06c2
jaehee kwon <100170572+hhhbbs1028@users.noreply.github.com>
2026-05-27 12:13:30 +0900

    [level 2] Title: 가장 큰 수, Time: 30.62 ms, Memory: 26.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,10 @@
+import math
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    answer = ''.join(numbers)
+    
+    if answer[0] == '0':
+        return '0'
+    
+    return answer
